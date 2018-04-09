1cc767bffe1004e90a62877d0fddd53197e403ed
Christophe Junke <junke.christophe@gmail.com>
2018-04-09 10:40:56 +0200

    Handle unknown implementations (for example)
    Signal an error if type-specifier-p is not available on current implementation.

--- src/typespecs.lisp
+++ src/typespecs.lisp
@@ -3,9 +3,11 @@
 (defun type-specifier-p (type-specifier)
   "Returns true if TYPE-SPECIFIER is a valid type specfiier."
   (or (documentation type-specifier 'type)
-      #+sbcl (sb-ext:valid-type-specifier-p type-specifier)
-      #+openmcl (ccl:type-specifier-p type-specifier)
-      #+ecl (c::valid-type-specifier type-specifier)))
+      (block nil
+        #+sbcl (return (sb-ext:valid-type-specifier-p type-specifier))
+        #+openmcl (return (ccl:type-specifier-p type-specifier))
+        #+ecl (return (c::valid-type-specifier type-specifier))
+        (error "Not implemented"))))
 
 (defun type-expand (type-specifier &optional env)
   "Expand TYPE-SPECIFIER in the lexical environment ENV."
